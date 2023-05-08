f80cd6d358da8c1c39aacdea327ce49854530231
YamikaniChabuka <bed-com-42-18@unima.ac.mw>
2023-05-08 15:30:30 +0200

    conditions in change pasword

--- change-password.php
+++ change-password.php
@@ -1,132 +1,105 @@
 <?php
-	// check if form is submitted
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		// retrieve form data
-		$old_password = $_POST['old_password'];
-		$new_password = $_POST['new_password'];
-		$confirm_password = $_POST['confirm_password'];
-		
-		// validate form data
-		if (empty($old_password)) {
-			echo "<p style='color: red;'>Please enter your old password.</p>";
-		} elseif (empty($new_password)) {
-			echo "<p style='color: red;'>Please enter your new password.</p>";
-		} elseif (empty($confirm_password)) {
-			echo "<p style='color: red;'>Please confirm your new password.</p>";
-		} elseif ($new_password != $confirm_password) {
-			echo "<p style='color: red;'>New password and confirm password do not match.</p>";
-		} else {
-			// TODO: check if old password matches current password in database
-			
-			// TODO: update password in database
-			
-			echo "<p style='color: green;'>Password changed successfully!</p>";
-		}
-	}
-	?>
+session_start();
 
+if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
+  include "connection.php";
+
+  if (isset($_POST['op']) && isset($_POST['np']) && isset($_POST['c_np'])) {
+
+    function validate($data){
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
+    $op = validate($_POST['op']);
+    $np = validate($_POST['np']);
+    $c_np = validate($_POST['c_np']);
+
+    if (empty($op)) {
+      header("Location: change-password.php?error=Old password is required");
+      exit();
+    } elseif (empty($np)) {
+      header("Location: change-password.php?error=New Password is required");
+      exit();
+    } elseif ($np !== $c_np) {
+      header("Location: change-password.php?error=The confirmation does not match");
+      exit();
+    } else {
+      // hashing the password
+      $op = md5($op);
+      $np = md5($np);
+      $id = $_SESSION['id'];
+
+      $sql = "SELECT password FROM users WHERE id='$id' AND password='$op'";
+
+      $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result) === 1) {
+        $sql_2 = "UPDATE users SET password='$np' WHERE id='$id'";
+        mysqli_query($conn, $sql_2);
+        header("Location: change-password.php?success=Your password has been changed successfully");
+        exit();
+      } else {
+        header("Location: change-password.php?error=Incorrect password");
+        exit();
+      }
+    }
+  } else {
+    header("Location: change-password.php");
+    exit();
+  }
+} else {
+  header("Location: Login_admin.php");
+  exit();
+}
+?> 
+
+
+
+
+
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Change Password</title>
-
-	<style>
-
-body {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 90vh;
-  margin: 0;
-  background-color: aqua;
-}
-
-h1 {
-  text-align: center;
-  margin-top: 0;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #fff;
-  padding: 50px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-  width: 30%;
-  margin: auto;
-}
-
-label {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 10px;
-  font-size: 18px;
-}
-
-input[type="password"] {
-  padding: 10px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 10px;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-  font-size: 18px;
-}
-
-input[type="submit"] {
-  background-color: #4CAF50;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-  font-size: 18px;
-}
-
-input[type="submit"]:hover {
-  background-color: #3e8e41;
-}
-
-p {
-  margin: 0;
-}
-
-p.error {
-  color: red;
-}
-
-p.success {
-  color: green;
-}
-
-input[type="date"] {
-  padding: 10px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 10px;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-  font-size: 18px;
-}
-
-	</style>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h1>Change Password</h1>
-	
-	<form method="POST">
-		<label for="old_password">Old Password:</label>
-		<input type="password" id="old_password" name="old_password" ><br><br>
-		<label for="new_password">New Password:</label>
-		<input type="password" id="new_password" name="new_password"><br><br>
-			<label for="confirm_password">Confirm New Password:</label>
-		<input type="password" id="confirm_password" name="confirm_password"><br><br>
-		<input type="submit" value="Change Password">
-	</form>
-</body>
+	 <form action="change-p.php" method="post">
+     	<h2>Change Password</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+     	<?php if (isset($_GET['success'])) { ?>
+     		<p class="success"><?php echo $_GET['success']; ?></p>
+     	<?php } ?>
+
+     	<label>Old password</label>
+     	<input type="text" name="op" placeholder="Old Password"><br>
+
+     	<label>New Password</label>
+     	<input type="password" name="np" placeholder="New Password"><br>
+
+     	<label>Confirm New Password</label>
+     	<input type="password" name="c_np" placeholder="Confirm New Password"><br>
+ 
+     	<button type="submit">CHANGE</button>
+          <a href="home.php" class="ca">HOME</a>
+     </form></body>
 </html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
