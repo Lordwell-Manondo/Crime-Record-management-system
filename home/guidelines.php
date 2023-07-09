4d7d46380f54f20c95121d58dd62367385857324
Lordwell-Manondo <bed-com-26-18@cc.ac.mw>
2023-07-09 20:45:57 +0200

    added comments to guidelines and updated powerpoint

--- home/guidelines.php
+++ home/guidelines.php
@@ -51,12 +51,15 @@
             background-color: blue;
         }
     </style>
-    <nav class="navbar navbar-expand-lg " style="background-color: rgb(0, 109, 139);;">
+    <!-- Navigation bar styling -->
+    <nav class="navbar navbar-expand-lg " style="background-color: rgb(0, 109, 139);">
     <div class="log">
+        <!-- Logo image -->
         <img src="../home/plog.PNG" style="height: 65px; width: 65px; margin-left: 5px; border-radius: 25px;  margin-top: 0px;">
     </div>
     <div>
-        <h1 style="color: white; text-align: center; ">Website Guidelines</h1>
+        <!-- Page title -->
+        <h1 style="color: white; text-align: center;">Website Guidelines</h1>
     </div>
 </nav>
 </head>
@@ -65,27 +68,29 @@
   session_start();
   // include database connection file
   include("../db/Connections.php");
-    // Retrieve guidelines from the database
-    $sql = "SELECT * FROM guidelines";
-    $result = mysqli_query($conn, $sql);
+  
+  // Retrieve guidelines from the database
+  $sql = "SELECT * FROM guidelines";
+  $result = mysqli_query($conn, $sql);
 
-    // Check if there are any guidelines
-    if (mysqli_num_rows($result) > 0) {
-        
-        echo "<div class='guidelines'>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<div class='guideline'>";
-            echo "<h3>" . $row['title'] . "</h3>";
-            echo "<p>" . $row['content'] . "</p>";
-            echo "</div>";
-        }
-        echo "</div>";
-    } else {
-        echo "<p>No guidelines found.</p>";
-    }
+  // Check if there are any guidelines
+  if (mysqli_num_rows($result) > 0) {
+      // Display each guideline
+      echo "<div class='guidelines'>";
+      while ($row = mysqli_fetch_assoc($result)) {
+          echo "<div class='guideline'>";
+          echo "<h3>" . $row['title'] . "</h3>";
+          echo "<p>" . $row['content'] . "</p>";
+          echo "</div>";
+      }
+      echo "</div>";
+  } else {
+      // No guidelines found
+      echo "<p>No guidelines found.</p>";
+  }
 
-    // Close the database connection
-    mysqli_close($conn);
-    ?>
+  // Close the database connection
+  mysqli_close($conn);
+  ?>
 </body>
 </html>
--- home/view-guidelines.php
+++ home/view-guidelines.php
@@ -51,41 +51,24 @@
             background-color: #45a049;
         }
     </style>
-    <nav class="navbar navbar-expand-lg " style="background-color: rgb(0, 109, 139);;">
+    <!-- Navigation bar styling -->
+    <nav class="navbar navbar-expand-lg " style="background-color: rgb(0, 109, 139);">
     <div class="log">
+        <!-- Logo image -->
         <img src="../home/plog.PNG" style="height: 65px; width: 65px; margin-left: 5px; border-radius: 25px;  margin-top: 0px;">
     </div>
     <div>
-        <h1 style="color: white; text-align: center; ">Website Guidelines</h1>
+        <!-- Page title -->
+        <h1 style="color: white; text-align: center; margin-left: 600px;">Guidelines</h1>
     </div>
 </nav>
     
 </head>
+<!-- External CSS and stylesheet links -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../login/login.css">
 
-    
-  <!-- Navbar -->
-
-  <nav class="navbar navbar-expand-lg " style="background-color: rgb(0, 109, 139);;">
-    <div class="log">
-      <img src="../home/plog.PNG" style="height: 65px; width: 65px; margin-left: 5px; border-radius: 25px;  margin-top: 0px;">
-      <h3>ADDING NEWS</h3>
-    </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-
-      
-         </ul>
-    </div>
-  </nav>
 <body>
     <?php
 
@@ -93,31 +76,32 @@ session_start();
 // include database connection file
 include("../db/Connections.php");
 
-    // Retrieve guidelines from the database
-    $sql = "SELECT * FROM guidelines";
-    $result = mysqli_query($conn, $sql);
+// Retrieve guidelines from the database
+$sql = "SELECT * FROM guidelines";
+$result = mysqli_query($conn, $sql);
 
-    // Check if there are any guidelines
-    if (mysqli_num_rows($result) > 0) {
-       
-        echo "<div class='guidelines'>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<div class='guideline'>";
-            echo "<h3>" . $row['title'] . "</h3>";
-            echo "<p>" . $row['content'] . "</p>";
-            echo "<form method='GET' action='update-guidelines.php'>";
-            echo "<input type='hidden' name='id' value='" . $row['id'] . "'>";
-            echo "<input type='submit' class='update-button' value='Update'>";
-            echo "</form>";
-            echo "</div>";
-        }
+// Check if there are any guidelines
+if (mysqli_num_rows($result) > 0) {
+    // Display each guideline
+    echo "<div class='guidelines'>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div class='guideline'>";
+        echo "<h3>" . $row['title'] . "</h3>";
+        echo "<p>" . $row['content'] . "</p>";
+        echo "<form method='GET' action='update-guidelines.php'>";
+        echo "<input type='hidden' name='id' value='" . $row['id'] . "'>";
+        echo "<input type='submit' class='update-button' value='Update'>";
+        echo "</form>";
         echo "</div>";
-    } else {
-        echo "<p>No guidelines found.</p>";
     }
+    echo "</div>";
+} else {
+    // No guidelines found
+    echo "<p>No guidelines found.</p>";
+}
 
-    // Close the database connection
-    mysqli_close($conn);
-    ?>
+// Close the database connection
+mysqli_close($conn);
+?>
 </body>
 </html>
